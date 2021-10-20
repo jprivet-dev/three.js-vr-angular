d5ed6e0811e7b607568c7e0706302825772272a6
jprivet-dev <jean.philippe.rivet@gmail.com>
2021-10-20 13:57:17 +0200

    feat(three-js-page.component): insert it in app.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ThreeJsPageComponent } from './three-js/containers/three-js-page/three-js-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ThreeJsPageComponent
+}, {
+  path: '**',
+  redirectTo: '',
+  pathMatch: 'full',
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
