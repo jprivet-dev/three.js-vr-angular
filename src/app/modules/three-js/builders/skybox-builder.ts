6b7c0e7afd10a14464a599a6d6e15b0dcb0a9d90
jprivet-dev <jean.philippe.rivet@gmail.com>
2021-10-23 16:57:22 +0200

    feat(skybox): fix path & private methods

--- src/app/modules/three-js/builders/skybox-builder.ts
+++ src/app/modules/three-js/builders/skybox-builder.ts
@@ -2,7 +2,7 @@ import { CubeTexture, CubeTextureLoader } from 'three';
 import { SkyboxDecorator } from '../decorators/skybox-decorator';
 
 export class SkyboxBuilder {
-  private assetsPath = '/assets/textures/skybox/';
+  private assetsPath = 'assets/textures/skybox/';
   private positionList = ['posx', 'negx', 'posy', 'negy', 'posz', 'negz'];
   private tag = '{pos}';
   private filenameWithTag = 'skybox_{pos}_512x512.jpg';
@@ -11,17 +11,17 @@ export class SkyboxBuilder {
     return new SkyboxDecorator(this.newCubeTexture());
   }
 
-  filenameList() {
-    return this.positionList.map((position) => this.filename(position));
-  }
-
-  filename(position: string): string {
-    return this.filenameWithTag.replace(this.tag, position);
-  }
-
-  newCubeTexture(): CubeTexture {
+  private newCubeTexture(): CubeTexture {
     return new CubeTextureLoader()
       .setPath(this.assetsPath)
       .load(this.filenameList());
+  }
+
+  private filenameList() {
+    return this.positionList.map((position) => this.filename(position));
+  }
+
+  private filename(position: string): string {
+    return this.filenameWithTag.replace(this.tag, position);
   }
 }
