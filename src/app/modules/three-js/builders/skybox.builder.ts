e763269bd80f302095073cc6a30e71e59935ccd8
jprivet-dev <jean.philippe.rivet@gmail.com>
2021-10-24 00:56:12 +0200

    refactor(skybox.builder): simplify

--- src/app/modules/three-js/builders/skybox.builder.ts
+++ src/app/modules/three-js/builders/skybox.builder.ts
@@ -2,28 +2,22 @@ import { CubeTexture, CubeTextureLoader } from 'three';
 import { SkyboxDecorator } from '../decorators';
 
 export abstract class SkyboxBuilder {
-  private static config = {
-    assetsPath: 'assets/textures/skybox/',
-    positionList: ['posx', 'negx', 'posy', 'negy', 'posz', 'negz'],
-    tag: '{pos}',
-    filenameWithTag: 'skybox_{pos}_512x512.jpg',
-  };
-
   static create(): SkyboxDecorator {
     return new SkyboxDecorator(this.newCubeTexture());
   }
 
   private static newCubeTexture(): CubeTexture {
     return new CubeTextureLoader()
-      .setPath(this.config.assetsPath)
+      .setPath('assets/textures/skybox/')
       .load(this.filenameList());
   }
 
   private static filenameList() {
-    return this.config.positionList.map((position) => this.filename(position));
+    const list = ['posx', 'negx', 'posy', 'negy', 'posz', 'negz'];
+    return list.map((position) => this.filename(position));
   }
 
   private static filename(position: string): string {
-    return this.config.filenameWithTag.replace(this.config.tag, position);
+    return ('skybox_{pos}_512x512.jpg').replace('{pos}', position);
   }
 }
